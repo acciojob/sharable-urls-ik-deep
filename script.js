2384f92621c071bcbbaf28a8071062958dbdae40
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-02-10 19:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+function changeUrl(){
+	const name = document.getElementById("name");
+	const year = document.getElementById("year");
+	const url = document.getElementById("url");
+
+	let newText = "https://localhost:8080/";
+	if(name.value.length >0 && year.value.length>0){
+		newText +="?"+name.value+"?"+year.value; 
+	}else if(name.value.length >0 && year.value.length<=0){
+	    newText+="?"+name.value;
+	}else if(name.value.length <=0 && year.value.length>0){
+		newText+="?"+year.value;
+	}else{
+		newText= "https://localhost:8080/";
+	}
+	url.innerHTML = newText;
+}
