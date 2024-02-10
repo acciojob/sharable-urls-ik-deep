f04c61063059fa1802784fd4f3c517d64439263f
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-02-10 22:31:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function changeUrl(){
 
 	let newText = "https://localhost:8080/";
 	if(name.value.length >0 && year.value.length>0){
-		newText +="?"+name.value+"?"+year.value; 
+		newText +="?name="+name.value+"?year="+year.value; 
 	}else if(name.value.length >0 && year.value.length<=0){
-	    newText+="?"+name.value;
+	    newText+="?name="+name.value;
 	}else if(name.value.length <=0 && year.value.length>0){
-		newText+="?"+year.value;
+		newText+="?year="+year.value;
 	}else{
 		newText= "https://localhost:8080/";
 	}
